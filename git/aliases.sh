0c63b0fe2e82d517d7d74b51dbca41a4783637bf
Matt Walters <mwalters8@gmail.com>
2013-03-13 18:07:43 -0400

    .

--- git/aliases.sh
+++ git/aliases.sh
@@ -2,4 +2,4 @@ alias gs='git status -sb'
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias ga='git add .'
-alias gc='git commit -m'
+alias gc='git commit -m "$@"'
