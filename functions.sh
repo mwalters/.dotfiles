059f7fbecc92b06280d0498149ebf107e5af2c19
Matt Walters <mwalters@captechconsulting.com>
2013-03-18 09:11:07 -0400

    Improves inline comments

--- functions.sh
+++ functions.sh
@@ -27,7 +27,7 @@ function fs() {
 	fi
 }
 
-# Start an HTTP server from a directory, optionally specifying the port
+# Start an HTTP server from a directory, optionally specifying the port; defaults to 8080
 function server() {
 	local port="${1:-8080}"
 	python -m SimpleHTTPServer $port
