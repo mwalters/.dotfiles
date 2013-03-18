813838fc8eb2d58858e1ee5a57336fbea44b2c0e
Matt Walters <mwalters@captechconsulting.com>
2013-03-18 09:11:33 -0400

    Changes formatting of prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -10,4 +10,3 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
 export PS1='\n[\e[0;32m\u@\h\[\033[0;37m\]\[\033[0m\]][\e[0;32m\@\[\033[0m\]] → \[\[\033[0;35m\]\w\[\033[0m\] $(parse_git_branch)\n\$ '
-
