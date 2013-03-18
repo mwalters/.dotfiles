082842acb8d32f6f50d47c6fa7d5824c2fa21881
Matt Walters <mwalters@captechconsulting.com>
2013-03-18 09:06:58 -0400

    Changes install script to be a little more intelligent
    Only attempt to rename files being replaced to their backup counterparts if they actually exist.  When renaming, do so safely.

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
-mv $HOME/.bashrc ~/.bashrc-bak
+if [ -f $HOME/.bashrc ];
+then
+    mv -i $HOME/.bashrc ~/.bashrc-bak
+fi
 ln -s $HOME/mswdotfiles/.bashrc ~/.bashrc
+
+
+if [ -f $HOME/mswdotfiles/.hushlogin ];
+then
+    mv -i $HOME/.hushlogin ~/.hushlogin-bak
+fi
 ln -s $HOME/mswdotfiles/.hushlogin ~/.hushlogin
+
+
 source $HOME/.bashrc
