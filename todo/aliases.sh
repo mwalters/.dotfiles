90174830fe31556c47f62989b4a8f2b253baf876
Matt Walters <mwalters8@gmail.com>
2013-04-10 21:07:36 -0400

    Updates to aliases to improve output and specify configuration

--- todo/aliases.sh
+++ todo/aliases.sh
@@ -1,6 +1,6 @@
-alias t='$HOME/mswdotfiles/todo/todo.sh'
+alias t='$HOME/mswdotfiles/todo/todo.sh -d $HOME/mswdotfiles/todo/todo.cfg'
 alias ta='t add'
 alias tarch='t archive'
 alias td='t do'
-alias tl='t list'
+alias tl='echo && t list && echo'
 alias tp='t pri'
