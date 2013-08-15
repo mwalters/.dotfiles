07dfeb0e6a4aabc846e927f1767d2e91d7156496
Matt Walters <me@mattwalters.net>
2013-08-14 20:41:28 -0400

    Moves some aliases to be part of git config

--- git/aliases.sh
+++ git/aliases.sh
@@ -6,8 +6,5 @@ alias ga='git add .'
 alias gc='git commit -a'
 alias gco='git checkout'
 alias gd='git diff'
-alias gsq='git rebase -i'
 alias gwtf='$HOME/.dotfiles/git/git-wtf'
 alias gbcp="git rev-parse --abbrev-ref HEAD | tr -d '\n' | pbcopy"
-alias gincoming="!(git fetch --quiet && git log --pretty=format:'%C(yellow)%h %C(white)- %C(red)%an %C(white)- %C(cyan)%d%Creset %s %C(white)- %ar%Creset' ..origin/master)"
-alias goutgoing="!(git fetch --quiet && git log --pretty=format:'%C(yellow)%h %C(white)- %C(red)%an %C(white)- %C(cyan)%d%Creset %s %C(white)- %ar%Creset' origin/master..)"
