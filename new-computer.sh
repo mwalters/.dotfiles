965fdc1859dbea71f5d0c385e05f41e0cb34b36f
Matt Walters <me@mattwalters.net>
2014-05-15 20:46:59 -0400

    Expands some apps to be installed

--- new-computer.sh
+++ new-computer.sh
@@ -2,6 +2,17 @@ brew tap phinze/cask
 brew install brew-cask
 brew cask install alfred
 brew cask alfred link
-brew cask install google-chrome
-brew cask install mou
+
 brew cask install bartender
+brew cask install divvy
+brew cask install caffeine
+brew cask install sublime-text
+brew cask install dropbox
+
+brew cask install virtualbox
+brew cask install vagrant
+
+brew cask install evernote
+brew cask install colorpicker
+brew cask install transmit
+brew cask install vlc
